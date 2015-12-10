d9b3143b7b15f6e381ebab6ef03dace6877f376d
Carl Colena <carl.colena@gmail.com>
2015-12-09 23:53:39 -0500

    i twied.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <complex>
+#include <valarray>
+#include <string.h>
 using namespace std;
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
 
 /***************************/
 int n = 5;  // width and height of the matrix
@@ -17,6 +23,45 @@ int getArrayVal(int *mat, int row, int col){
 void setArrayVal(int *mat, int row, int col, int val){
 	mat[row + col*n] = val;
 	}
+	
+	
+void fft(CArray& x)
+{
+    const size_t N = x.size();
+    if (N <= 1) return;
+ 
+    // divide
+    CArray even = x[std::slice(0, N/2, 2)];
+    CArray  odd = x[std::slice(1, N/2, 2)];
+ 
+    // conquer
+    fft(even);
+    fft(odd);
+ 
+    // combine
+    for (size_t k = 0; k < N/2; ++k)
+    {
+        Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
+        x[k    ] = even[k] + t;
+        x[k+N/2] = even[k] - t;
+    }
+}
+
+void ifft(CArray& x)
+{
+    // conjugate the complex numbers
+    x = x.apply(std::conj);
+ 
+    // forward fft
+    fft( x );
+ 
+    // conjugate the complex numbers again
+    x = x.apply(std::conj);
+ 
+    // scale the numbers
+    x /= x.size();
+}
+		
 	
 
 
@@ -67,6 +112,124 @@ int main(){
 		}
 		result[i] = tempsum;
 	}
+	
+	// Matrix Multi w/ FFT
+	
+	unsigned int a = 2*n-1;
+	unsigned int b = n-1;
+	unsigned int c = a+b;
+
+	//round to nearest power of 2 for efficient fft
+	c--;
+	c |= c >> 1;
+	c |= c >> 2;
+	c |= c >> 4;
+	c |= c >> 8;
+	c |= c >> 16;
+	c++;
+	
+	
+	// initialize arrays for fft. c is next highest power of 2 of our polys for fft.
+	int *tpoly = new int[c]; //toeplitz 
+	int *ppoly = new int[c]; //vector
+	int *rpoly = new int[c]; //result
+	int i = 0;
+	int u = b;
+	
+	//making the tpoly array. 
+	while(u >= 0){
+		cout << u << endl;
+		tpoly[i] = getArrayVal(Toeplitz, u, 0);
+		u--;
+		i++;
+		}
+	u = 1;
+	while(u < n-1){
+		tpoly[i] = getArrayVal(Toeplitz, 0, u);
+		u++;
+		i++;
+		}
+	while(i <= c-1){
+		tpoly[i] = 0;
+		i++;
+		}
+		
+	// making the ppoly array
+	i = 0;	
+	while(i <= n-1){
+		ppoly[i] = pmatrix[n-1-i];
+		i++;
+		}
+	while(i <= c-1){
+		ppoly[i] = 0;
+		i++;
+		}	
+		
+	cout << " TARR: ";	
+	for (int lol = 0; lol < c; lol++){
+		cout << tpoly[lol] << " ";
+		}
+	cout << endl;
+	cout << " PARR: ";	
+	for (int lol = 0; lol < c; lol++){
+		cout << ppoly[lol] << " ";
+		}		
+	cout << endl;
+	
+	
+	Complex *tfft = new Complex[c];
+	for (int lol = 0; lol < c; lol++){
+		tfft[lol] = tpoly[lol];
+		}
+    CArray tft(tfft, c);
+	
+	
+	Complex *pfft = new Complex[c];
+	for (int lol = 0; lol < c; lol++){
+		pfft[lol] = ppoly[lol];
+		}
+    CArray pft(pfft, c);
+
+	
+	
+	Complex *rfft = new Complex[c];
+	for (int lol = 0; lol < c; lol++){
+		rfft[lol] = 0;
+		}
+    CArray rft(rfft, c);
+
+	
+	fft (tft);
+	fft (pft);
+	
+		for (int lol = 0; lol < c; lol++){
+		cout << tft[lol] << " ";
+		}
+		cout << endl;
+		
+		for (int lol = 0; lol < c; lol++){
+		cout << pft[lol] << " ";
+		}
+		cout << endl;
+			
+			
+			
+	for(int i = 0; i < c; i++){
+		rft[i] = tft[i]*pft[i];
+		}
+		for (int lol = 0; lol < c; lol++){
+		cout << rft[lol] << " ";
+		}
+		cout << endl;
+		
+	ifft (rft);
+
+	cout << " OUTPUT: ";	
+	for (int lol = 0; lol < c; lol++){
+		cout << rft[lol] << " ";
+		}		
+	cout << endl;
+	
 	
 	
 	// prints topelitz matrix
